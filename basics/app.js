3f784e2f779f4c9b4f1e0acea1117d8e289ba41d
Vivek Joy <vivekjne@gmail.com>
2020-09-30 12:53:00 +0530

    basic component and template

--- basics/app.js
+++ basics/app.js
@@ -2,7 +2,17 @@ const CounterApp = {
     data() {
       return {
         counter: 0,
-        message:''
+        message:'',
+        seen:true,
+        moviesList:[
+            {id:1,name:"Star Wars 1"},
+            {id:2,name:"Star Wars 2"},
+            {id:3,name:"Star Wars 3"},
+            {id:4,name:"Star Wars 4"},
+            {id:5,name:"Star Wars 5"},
+
+        ],
+        movieName:""
       }
     },
     mounted() {
@@ -13,8 +23,22 @@ const CounterApp = {
     methods:{
         reverseMessage(){
             this.message = this.message.split('').reverse().join('')
+        },
+        toggleDiv(){
+            this.seen = !this.seen
+        },
+        onSubmit(e){
+            this.moviesList.push({id:this.moviesList.length,name:this.movieName})
+            this.movieName = ""
         }
     }
   }
 
-  Vue.createApp(CounterApp).mount('#app')
+  const app = Vue.createApp(CounterApp)
+
+  app.component('movie-item', {
+    props: ['movie'],
+    template: `<li>{{ movie.name }}</li>`
+  })
+
+  app.mount('#app')
